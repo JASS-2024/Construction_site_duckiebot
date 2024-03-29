04c89a734cd08a86ad9e4eb7552a62d69ae9ef46
limarkdl <ikostin@uclan.ac.uk>
2024-03-29 11:20:26 +0200

    Working mqtt + Fixes

--- packages/mqtt/src/mqtt_node.py
+++ packages/mqtt/src/mqtt_node.py
@@ -1,93 +1,92 @@
 #!/usr/bin/env python3
 import numpy as np
 import rospy
-import cv2
-
-from duckietown.dtros import DTROS, NodeType, TopicType, DTParam, ParamType
-from cv_bridge import CvBridge
-from sensor_msgs.msg import CompressedImage, Image
-from duckietown_msgs.msg import BoolStamped
-from duckietown_msgs.srv import SetFSMState, SetFSMStateResponse, ChangePattern
-
-from std_msgs.msg import Float32MultiArray
-
-from paho.mqtt import client as mqtt
-
 import json
+from paho.mqtt import client as mqtt
+from paho.mqtt.enums import CallbackAPIVersion
+from std_msgs.msg import Float32MultiArray
+from duckietown.dtros import DTROS, NodeType, DTParam, ParamType
 
-
-# TODO: 'Coordinates for parking slots x1,y1,x2,y2,x3,y3,x4,y4 to share parking slot's corners'
-
+from datetime import datetime
 class MQTTNode(DTROS):
-    """Retrieves data from OptiTrack via MQTT and publishes it in [x, y, theta, timestamp]: Float32MultiArray. Timestamp
-    is in nanosecond.
-
-    Configuration:
-        ~object_id (:obj:`Int32`): ID of the robot in OptiTrack system
-
-        ~mqtt_broker_ip (:obj:`String`): Host's IP address
-
-        ~mqtt_topic (:obj:`String`): Name of the required topic in MQTT broker
-
-    Publisher:
-        ~telemetry (:obj:`Float32MultiArray`): The data from OptiTrack through MQTT. [x, y, theta, timestamp]
-    """
-
     def __init__(self, node_name):
-        # Initialize the DTROS parent class
-        super(MQTTNode, self).__init__(node_name=node_name, node_type=NodeType.DATA_PROCESSING)
+        """Retrieves data from OptiTrack via MQTT and publishes it in [x, y, theta, timestamp]: Float32MultiArray. Timestamp
+          is in nanosecond.
 
-        # Add the node parameters to the parameters dictionary
-        self.params = dict()
-        self.params["object_id"] = DTParam("~object_id", param_type=ParamType.INT32)
-        self.params["mqtt_broker_ip"] = DTParam("~mqtt_broker_ip", param_type=ParamType.INT32)
-        self.params["mqtt_topic"] = DTParam("~mqtt_topic", param_type=ParamType.INT32)
+          Configuration:
+              ~object_name (:obj:`String`): ID of the robot in OptiTrack system
 
-        # Getting predefined params
-        self.mqtt_broker_ip = self.mqtt_broker_ip
-        self.mqtt_topic = self.mqtt_topic
+              ~mqtt_broker_ip (:obj:`String`): Host's IP address
+
+              ~mqtt_topic (:obj:`String`): Name of the required topic in MQTT broker
+
+          Publisher:
+              ~telemetry (:obj:`Float32MultiArray`): The data from OptiTrack through MQTT. [x, y, theta, timestamp]
+          """
+
+        super(MQTTNode, self).__init__(node_name=node_name, node_type=NodeType.PERCEPTION)
+        self.params = {
+            "object_name": DTParam("~object_name", param_type=ParamType.STRING),
+            "mqtt_broker_ip": DTParam("~mqtt_broker_ip", param_type=ParamType.STRING),
+            "mqtt_topic": DTParam("~mqtt_topic", param_type=ParamType.STRING)
+        }
+        self.mqtt_broker_ip = rospy.get_param('~mqtt_broker_ip')
+        self.mqtt_topic = rospy.get_param('~mqtt_topic')
 
         # MQTT Client setup
-        self.mqtt_client = mqtt.Client()
+        self.mqtt_client = mqtt.Client(callback_api_version=CallbackAPIVersion.VERSION1) # ATTENTION: This API version (1) is deprecated. Currentrly using just because it works
         self.mqtt_client.on_connect = self.on_connect
-        self.mqtt_client.on_message = self.on_msg
-        self.log(f'Connecting to the {self.mqtt_broker_ip}')
+        self.mqtt_client.on_message = self.on_message
+        self.log(f'Connecting to {self.mqtt_broker_ip}')
         self.mqtt_client.connect(self.mqtt_broker_ip, 1883, 60)
         self.mqtt_client.loop_start()
 
-        # Construct publishers
-        self.pub_telemetry_params = rospy.Publisher(
-            "~telemetry", Float32MultiArray, queue_size=1, dt_topic_type=TopicType.SERVICE
-        )
-
+        # Construct publisher
+        self.pub_telemetry = rospy.Publisher("~telemetry", Float32MultiArray, queue_size=1)
         self.log("Initialized!")
 
-    # On successful connect
-    def on_connect(self, rc):
-        print("Connected to OptiTrack MQTT Broker with code " + str(rc))
-        self.mqtt_client.subscribe(self.mqtt_topic)
+    def on_connect(self, client, userdata, flags, rc):
+        if rc == 0:
+            self.log("Connected to MQTT Broker!")
+            self.mqtt_client.subscribe(self.mqtt_topic)
+        else:
+            self.log(f"Failed to connect, return code {rc}")
 
-
-    # Assuming that we receive msg in a 'decodable'format and we're able to decode it to JSON with
-    # 'position' key - value for required object
-
-    # TODO: 'Make sure that we receive timestamp in nanoseconds. For this we can use something like rospy.time.now toNanoSec'
-    # On receiving message
-    def on_msg(self, msg):
-        print(f"!!! Received message on topic {msg.topic} !!!")
+    def on_message(self, client, userdata, msg):
         try:
             data = json.loads(msg.payload.decode())
+            coordinates = data['data']['coordinates']
+            yaw_in_radians = np.deg2rad(coordinates['yaw'])
+            timestamp = data['data']['timestamp']
+            nanoseconds = self.getNanoSec(timestamp)
             telemetry_msg = Float32MultiArray()
-            telemetry_msg.data = [data['position']['x'], data['position']['y'], data['position']['theta'], data['position']['timestamp']]
-            self.pub_telemetry_params.publish(telemetry_msg)
+            telemetry_msg.data = [coordinates['x'], coordinates['y'], yaw_in_radians, timestamp]
+            self.pub_telemetry.publish(telemetry_msg)
+            print(telemetry_msg.data)
+            print(telemetry_msg)
         except Exception as e:
-            print(f"Something upalo with error: {e}")
+            rospy.logerr(f"Error in on_message: {e}")
 
 
+    def getNanoSec(self, timestamp_str):
+        timestamp_dt = datetime.strptime(timestamp_str, "%Y-%m-%dT%H:%M:%S.%f")
+        posix_timestamp = timestamp_dt.timestamp()
+        ros_time = rospy.Time.from_sec(posix_timestamp)
+
+        nanoseconds = int(timestamp_dt.timestamp() * 1e9)
+
+        return nanoseconds
+
+    # DEBUGGING
+    # def on_message(self, client, userdata, msg):
+    #     print(f"Message received: {msg.payload.decode()} on topic {msg.topic}")
+
+    def on_shutdown(self):
+        self.mqtt_client.loop_stop()
+        self.mqtt_client.disconnect()
+        super(MQTTNode, self).on_shutdown()
 
 
 if __name__ == "__main__":
-    # Initialize the node
-    lane_controller_node = MQTTNode(node_name="mqtt_node")
-    # Keep it spinning
+    mqtt_node = MQTTNode(node_name="mqtt_node")
     rospy.spin()
