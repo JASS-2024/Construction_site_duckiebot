dd961d3c7b9b1dadf836bf768800ef7064c39990
alpdk <alexander@podkopaev.net>
2024-03-29 11:28:56 +0200

    Added comments

--- packages/parking/src/parking_node.py
+++ packages/parking/src/parking_node.py
@@ -16,8 +16,7 @@ class ParkingNode(DTROS):
     def __init__(self, node_name):
         super(ParkingNode, self).__init__(node_name=node_name, node_type=NodeType.DRIVER)
 
-        # Add the node parameters to the parameters dictionary
-        # TODO: MAKE TO WORK WITH NEW DTROS PARAMETERS
+        # Initialize params
         self.params = dict()
 
         self.params["move_speed"] = DTParam("~move_speed", param_type=ParamType.FLOAT, min_value=0, max_value=1)
@@ -25,36 +24,12 @@ class ParkingNode(DTROS):
         self.params["move_time"] = DTParam("~move_time", param_type=ParamType.FLOAT, min_value=0, max_value=1)
         self.params["rotation_time"] = DTParam("~rotation_time", param_type=ParamType.FLOAT, min_value=0, max_value=1)
 
-        # Initialize variables
-        # self.bridge = CvBridge()
-
-        # TODO: publisher
-
-        # Construct publishers
-        # self.pub_obstacle_image = rospy.Publisher(
-        #     "~debug/segmentation/compressed", CompressedImage, queue_size=1, dt_topic_type=TopicType.DEBUG
-        # )
-        #
-        # self.pub_stop = rospy.Publisher(
-        #     "~stop_request", BoolStamped, queue_size=1, dt_topic_type=TopicType.PERCEPTION
-        # )
-
-        # Construct subscribers
-        # self.duckie_image = rospy.Subscriber(
-        #     "~duckie_image",  CompressedImage, self.processImageMessage, buff_size=10000000, queue_size=1
-        # )
-
-        # TODO:
-        # make some decisions on format of the data you will get from ivan
-        # and subscribe to it when you will decide on the names and formats
-
-        # self.duckie_image = rospy.Subscriber(
-        #     "~",  CompressedImage, self.processImageMessage, buff_size=10000000, queue_size=1
-        # )
-
+        # Open subscription for type of movement
         self.make_move_name = rospy.Subscriber(
             '~make_move_name', String, self.make_move, queue_size=1
         )
+
+        # Open publisher for wheels
 
         vehicle_name = os.environ['VEHICLE_NAME']
         wheels_topic = f"/{vehicle_name}/wheels_driver_node/wheels_cmd"
@@ -63,6 +38,8 @@ class ParkingNode(DTROS):
             wheels_topic, WheelsCmdStamped, queue_size=1, dt_topic_type=TopicType.DEBUG
         )
 
+        # Open publisher for stop signal
+
         self.move_stop = rospy.Publisher(
             "~move_stop", BoolStamped, queue_size=1, dt_topic_type=TopicType.DEBUG
         )
@@ -70,51 +47,40 @@ class ParkingNode(DTROS):
         self.log("Initialized!")
 
     def make_move(self, msg):
-        print("Start moving")
+        if msg.data == "FORWARD": # Move forward
 
-        print("Current Values:")
-        print("    move_speed: ", self.params["move_speed"].value)
-        print("    rotation_speed: ", self.params["rotation_speed"].value)
-        print("    move_time: ", self.params["move_time"].value)
-        print("    rotation_time:", self.params["rotation_time"].value)
-
-        if msg.data == "FORWARD":
-            print("Move forward")
             message = WheelsCmdStamped(vel_left=self.params["move_speed"].value,
                                        vel_right=self.params["move_speed"].value)
             self.wheel_publisher.publish(message)
             time.sleep(self.params["move_time"].value)
 
-        elif msg.data == "BACK":
-            print("Move back")
+        elif msg.data == "BACK": # Move Back
 
             message = WheelsCmdStamped(vel_left=-self.params["move_speed"].value,
                                        vel_right=-self.params["move_speed"].value)
             self.wheel_publisher.publish(message)
             time.sleep(self.params["move_time"].value)
 
-        elif msg.data == "RIGHT":
-            print("Rotate right")
+        elif msg.data == "RIGHT": # Rotate Right
 
             message = WheelsCmdStamped(vel_left=self.params["rotation_speed"].value,
                                        vel_right=-self.params["rotation_speed"].value)
             self.wheel_publisher.publish(message)
             time.sleep(self.params["rotation_time"].value)
 
-        elif msg.data == "LEFT":
-            print("Rotate Left")
+        elif msg.data == "LEFT": # Rotate Left
 
             message = WheelsCmdStamped(vel_left=-self.params["rotation_speed"].value,
                                        vel_right=self.params["rotation_speed"].value)
             self.wheel_publisher.publish(message)
             time.sleep(self.params["rotation_time"].value)
 
-        print("Stop move or rotate")
+        # Stop any move
 
         message = WheelsCmdStamped(vel_left=0, vel_right=0)
         self.wheel_publisher.publish(message)
 
-        print("Send BoolStamped True")
+        # Inform about movement stop
 
         msg = BoolStamped()
         msg.header.stamp = rospy.Time.now()
